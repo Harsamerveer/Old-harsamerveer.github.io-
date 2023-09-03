2e6c1f86c1ee7f1468eae10b8ccbd3b89ab69c69
Harsamerveer <harsamerveersingh@gmail.com>
2023-09-03 16:20:54 -0700

    Added secrets.yaml

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from decouple import config
 # Access the API key using .env method
 api_key2 = config('API_KEY')
 # Access the API key using os .env method
-api_key = os.environ["API_KEY"]
+api_key = os.environ['API_KEY']
 
 class Config(object):
     DEBUG = True
